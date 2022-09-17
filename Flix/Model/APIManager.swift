c65c3a26bfd2a5dd13f4f9a9b86aa717e96fee0b
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-09-16 22:37:31 -0700

    included alamofire 3rd party library for image loading

--- Flix/Model/APIManager.swift
+++ Flix/Model/APIManager.swift
@@ -11,10 +11,10 @@ struct APIManager {
     
     let APIKey: String = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
     
-    func getMovies(apikey: String) -> [String: Any] {
+    func getMovies() -> [[String: Any]] {
         
         // initialize it so xcode doesn't cry
-        var dataDictionary = [String: Any]()
+        var movies = [[String: Any]]()
         
         let url = URL(string: APIKey)!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -24,17 +24,19 @@ struct APIManager {
              if let error = error {
                     print(error.localizedDescription)
              } else if let data = data {
-                    dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                  
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
                     // TODO: Reload your table view data
-                 
+                    
+                 movies = dataDictionary["results"] as! [[String: Any]]
+                 // print(movies)
              }
         }
         task.resume()
         
-        return dataDictionary
+        return movies
     }
     
     var getApiKey: String {
